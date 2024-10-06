444c55f35d13f78416e20fbcfba5417a5c25e00c
GouthamNP <107291366+GouthamOfTheNP@users.noreply.github.com>
2024-10-06 11:57:51 -0500

    Add files via upload

--- sendemailpy3.py
+++ sendemailpy3.py
@@ -1,32 +1,50 @@
 import smtplib
 from email.message import EmailMessage
-import os
+from email.mime.base import MIMEBase
+from email import encoders
+
 
 class EmailSender():
-	def __init__(self, subject, content, receiver, username, password):
+	def __init__(self, subject, content, receiver, username, password, filepath=None):
 		self.subject = subject
 		self.content = content
 		self.receiver = receiver
 		self.username = username
 		self.password = password
+		self.filepath = filepath
 	
 	def send_gmail(self):
-		send_gmail(self.subject, self.content, self.receiver, self.username, self.password)
+		send_gmail(self.subject, self.content, self.receiver, self.username, self.password, self.filepath)
 	
 	def send_outlook_email(self):
-		send_outlook_email(self.subject, self.content, self.receiver, self.username, self.password)
+		send_outlook_email(self.subject, self.content, self.receiver, self.username, self.password, self.filepath)
 	
 	def send_yahoo_email(self):
-		send_yahoo_email(self.subject, self.content, self.receiver, self.username, self.password)
+		send_yahoo_email(self.subject, self.content, self.receiver, self.username, self.password, self.filepath)
 	
 	def send_proton_mail(self):
-		send_proton_email(self.subject, self.content, self.receiver, self.username, self.password)
+		send_proton_email(self.subject, self.content, self.receiver, self.username, self.password, self.filepath)
 
-def send_gmail(subject, content, receiver, username, password):
+
+def send_gmail(subject, content, receiver, username, password, filepath=None):
 	print("Send email function started")
 	email_message = EmailMessage()
 	email_message["Subject"] = subject
 	email_message.set_content(content)
+	if filepath is not None:
+		try:
+			with open(filepath, 'rb') as file:
+				mime_base = MIMEBase('application', 'octet-stream')
+				mime_base.set_payload(file.read())
+				
+			encoders.encode_base64(mime_base)
+			
+			mime_base.add_header('Content-Disposition', f'attachment; filename={filepath.split("/")[-1]}')
+			
+			email_message.add_attachment(mime_base.get_payload(decode=True), maintype='application', subtype='octet-stream', filename=filepath.split("/")[-1])
+
+		except Exception as e:
+			print(f"Failed to attach the file: {e}")
 
 	gmail = smtplib.SMTP("smtp.gmail.com", 587)
 	gmail.ehlo()
@@ -36,11 +54,26 @@ def send_gmail(subject, content, receiver, username, password):
 	gmail.quit()
 	print("Send email function ended")
 
-def send_outlook_email(subject, content, receiver, username, password):
+
+def send_outlook_email(subject, content, receiver, username, password, filepath=None):
 	print("Send email function started")
 	email_message = EmailMessage()
 	email_message["Subject"] = subject
 	email_message.set_content(content)
+	if filepath is not None:
+		try:
+			with open(filepath, 'rb') as file:
+				mime_base = MIMEBase('application', 'octet-stream')
+				mime_base.set_payload(file.read())
+				
+			encoders.encode_base64(mime_base)
+			
+			mime_base.add_header('Content-Disposition', f'attachment; filename={filepath.split("/")[-1]}')
+			
+			email_message.add_attachment(mime_base.get_payload(decode=True), maintype='application', subtype='octet-stream', filename=filepath.split("/")[-1])
+
+		except Exception as e:
+			print(f"Failed to attach the file: {e}")
 	
 	outlook = smtplib.SMTP("smtp-mail.outlook.com", 587)
 	outlook.ehlo()
@@ -50,11 +83,26 @@ def send_outlook_email(subject, content, receiver, username, password):
 	outlook.quit()
 	print("Send email function ended")
 
-def send_yahoo_email(subject, content, receiver, username, password):
+
+def send_yahoo_email(subject, content, receiver, username, password, filepath=None):
 	print("Send email function started")
 	email_message = EmailMessage()
 	email_message["Subject"] = subject
 	email_message.set_content(content)
+	if filepath is not None:
+		try:
+			with open(filepath, 'rb') as file:
+				mime_base = MIMEBase('application', 'octet-stream')
+				mime_base.set_payload(file.read())
+				
+			encoders.encode_base64(mime_base)
+			
+			mime_base.add_header('Content-Disposition', f'attachment; filename={filepath.split("/")[-1]}')
+			
+			email_message.add_attachment(mime_base.get_payload(decode=True), maintype='application', subtype='octet-stream', filename=filepath.split("/")[-1])
+
+		except Exception as e:
+			print(f"Failed to attach the file: {e}")
     
 	yahoo = smtplib.SMTP("smtp.mail.yahoo.com", 465)
 	yahoo.ehlo()
@@ -64,11 +112,26 @@ def send_yahoo_email(subject, content, receiver, username, password):
 	yahoo.quit()
 	print("Send email function ended")
 
-def send_proton_email(subject, content, receiver, username, password):
+
+def send_proton_email(subject, content, receiver, username, password, filepath=None):
 	print("Send email function started")
 	email_message = EmailMessage()
 	email_message["Subject"] = subject
 	email_message.set_content(content)
+	if filepath is not None:
+		try:
+			with open(filepath, 'rb') as file:
+				mime_base = MIMEBase('application', 'octet-stream')
+				mime_base.set_payload(file.read())
+				
+			encoders.encode_base64(mime_base)
+			
+			mime_base.add_header('Content-Disposition', f'attachment; filename={filepath.split("/")[-1]}')
+			
+			email_message.add_attachment(mime_base.get_payload(decode=True), maintype='application', subtype='octet-stream', filename=filepath.split("/")[-1])
+
+		except Exception as e:
+			print(f"Failed to attach the file: {e}")
     
 	proton = smtplib.SMTP("smtp.protonmail.ch", 587)
 	proton.ehlo()
